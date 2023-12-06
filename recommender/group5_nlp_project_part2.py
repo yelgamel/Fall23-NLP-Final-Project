e3dac199c1e4dda14da79211f5f4acc9efdc04d7
Maciel Lopez <maciel.lopez.iii@gmail.com>
2023-12-05 22:51:51 -0700

    latest

--- recommender/group5_nlp_project_part2.py
+++ recommender/group5_nlp_project_part2.py
@@ -154,7 +154,7 @@ def recommend():
 
     # when list is empty, provide recommendations for all media types
     if not requested_media:
-        requested_media = ["movies", "tv-series", "video-games"]
+        requested_media = ["movie", "tv series", "video game"]
 
     item_matrix = tfidf_matrix
     vectorizer = tfidf_vec
@@ -171,7 +171,7 @@ def recommend():
         cosine_similarities = calculate_cosine_similarity(user_data, item_matrix, vectorizer)
 
         # Get the indices of the top recommendations
-        similar_indices = cosine_similarities[0].argsort()[-2:-num_recommendations-2:-1]
+        similar_indices = cosine_similarities[0].argsort()[-2::-1]
     else:
         user_data = " ".join([title, key_words])
 
@@ -179,50 +179,85 @@ def recommend():
         cosine_similarities = calculate_cosine_similarity(user_data, item_matrix, vectorizer)
 
         # Get the indices of the top recommendations
-        similar_indices = cosine_similarities[0].argsort()[:-num_recommendations-1:-1]
+        similar_indices = cosine_similarities[0].argsort()[::-1]
 
-    # Convert to float
-    item_df['ranking'] = pd.to_numeric(
-        item_df['ranking'], errors='coerce')
+    # # Convert to float
+    # item_df['ranking'] = pd.to_numeric(
+    #     item_df['ranking'], errors='coerce')
 
-    item_df['runtime'] = pd.to_numeric(
-        item_df['runtime'], errors='coerce')
+    # item_df['runtime'] = pd.to_numeric(
+    #     item_df['runtime'], errors='coerce')
 
-    item_df['votes'] = pd.to_numeric(
-        item_df['votes'], errors='coerce')
+    # item_df['votes'] = pd.to_numeric(
+    #     item_df['votes'], errors='coerce')
 
-    # Filter the data based on user preferences and remove rows with NaN values
-    if not ((ranking is None) or (year is None) or (rating is None) or (runtime is None) or (num_votes is None)):
+    # # Filter the data based on user preferences and remove rows with NaN values
+    # if not ((ranking is None) or (year is None) or (rating is None) or (runtime is None) or (num_votes is None)):
 
-        filtered_df = item_df[
-            (item_df['ranking'] >= ranking) &
-            (item_df['runtime'] <= runtime) &
-            (item_df['year'] == year) &
-            (item_df['rating'] >= rating) &
-            (item_df['votes'] >= num_votes)].dropna()
-    else:
-        filtered_df = item_df.dropna()
+    #     filtered_df = item_df[
+    #         (item_df['ranking'] >= ranking) &
+    #         (item_df['runtime'] <= runtime) &
+    #         (item_df['year'] == year) &
+    #         (item_df['rating'] >= rating) &
+    #         (item_df['votes'] >= num_votes)].dropna()
+    # else:
+    #     filtered_df = item_df.dropna()
 
     recommendations = []
+    ids = []
+    media_types = []
     for index in similar_indices:
+        if len(recommendations) >= num_recommendations:
+            break
+
         title = item_df['title'].iloc[index]
-        if title not in filtered_df['title'].values:
+        media_type = item_df['type'].iloc[index]
+
+        if media_type in requested_media:
             recommendations.append(title)
+            ids.append(index)
+            if item_df['type'].iloc[index] == 'video game':
+                media_types.append("Video Game")
+            elif item_df['type'].iloc[index] == 'movie':
+                media_types.append('Movie')
+            else:
+                media_types.append('TV Series')
 
-    enter_types = []
-    for index in similar_indices:
-        if item_df['type'].iloc[index] == 'video game':
-            enter_types.append("Video Game")
-        elif item_df['type'].iloc[index] == 'movie':
-            enter_types.append('Movie')
-        else:
-            enter_types.append('TV Series')
+    return render_template(
+        'recommendations.html',
+        recommendations=zip(recommendations, ids, media_types),
+        num_recommendations=num_recommendations,
+        title=title_orig
+    )
 
-    return render_template('recommendations.html',
-                           recommendations=zip(recommendations, similar_indices, enter_types),
-                           num_recommendations=num_recommendations,
-                           title=title_orig
-                           )
+@app.route('/search', methods=['GET'])
+def search():
+    try:
+        media_id = request.args.get('media_id', default=None, type=int)
+        print("Searching for ", media_id)
+    except ValueError as e:
+        print("Could not process Request: ", e)
+
+    item_df = media
+    title = item_df['title'].iloc[media_id]
+    actors = ", ".join([item_df['actor-1'].iloc[media_id],
+                        item_df['actor-2'].iloc[media_id],
+                        item_df['actor-3'].iloc[media_id],
+                        item_df['actor-4'].iloc[media_id]])
+    director = item_df['director'].iloc[media_id]
+    description = item_df['description'].iloc[media_id]
+    genre = item_df['genre'].iloc[media_id]
+
+
+    return render_template(
+        'search.html',
+        media_id=media_id,
+        title=title,
+        actors=actors,
+        director=director,
+        description=description,
+        genre=genre
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
